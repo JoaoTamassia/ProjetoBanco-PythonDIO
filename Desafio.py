48e00098ea2fdd6be5ee39c20fa2efb316b6d210
João Tamassia <joaotamassia@alunos.utfpr.edu.br>
2024-04-05 15:28:53 -0300

    Projeto concluído

---
+++ Desafio.py
@@ -0,0 +1,55 @@
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        print("Você selecionou a opção ** DEPÓSITO ** !")
+        valor_deposito = float(input("Digite o valor a ser depositado: "))
+        if valor_deposito > 0:
+            saldo += valor_deposito
+            extrato += f"Depósito: R$ {valor_deposito:.2f}\n"
+        else:
+            print("Valor inválido, por favor digite um valor positivo !")
+
+    elif opcao == "s":
+        print("Você selecionou a opção ** SAQUE ** !")
+        valor_saque = float(input("Digite o valor a ser sacado: "))
+        if valor_saque <= 500 and numero_saques < LIMITE_SAQUES and valor_saque <= saldo:
+            saldo -= valor_saque
+            numero_saques += 1
+            print(f"Você realizou o saque de R${valor_saque} e seu saldo atual é de {saldo}")
+            extrato += f"Saque: R$ {valor_saque:.2f}\n"
+        elif numero_saques >= LIMITE_SAQUES:
+            print("Você excedeu o limite de saques diário ! Volte amanhã")
+        elif valor_saque > 500:
+            print("O limite do valor sacável é de R$ 500, tente sacar um valor inferior")
+        elif valor_saque > saldo:
+            print("Você não possui saldo suficiente para realizar esta operação !")
+
+    elif opcao == "e":
+        print("Você selecionou a opção ** EXTRATO ** !")
+        if extrato == "":
+            print("Não foram realizadas movimentações !")
+        else:
+            print(extrato)
+        
+    
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
